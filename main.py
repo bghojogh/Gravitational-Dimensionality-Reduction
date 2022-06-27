8134f9799d0425120a9e490b2cda5e07fdfd415e
unknown <bghojogh@uwaterloo.ca>
2022-06-26 20:25:36 -0400

    Added some trials

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import numpy as np
+from sklearn.decomposition import PCA
 
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+print(digits.data.shape)
+
+import matplotlib.pyplot as plt
+
+plt.matshow(digits.images[0])
+plt.gray()
+plt.show()
